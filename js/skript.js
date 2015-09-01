44b2cae5c4ffb3c91d4c75ef30108c1be844eea8
Vasiliy <i-hitman47@yandex.ru>
2015-09-01 23:40:51 +0300

    112233

--- js/skript.js
+++ js/skript.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
+    var myDataRef = new Firebase('https://iwish.firebaseio.com/');
     console.log("started");
+   $('#messageInput').keypress(function (e) {
+        if (e.keyCode == 13) {
+          var name = $('#nameInput').val();
+          var text = $('#messageInput').val();
+          myDataRef.push({name: name, text: text});
+          $('#messageInput').val('');
+        }
+      });
+      myDataRef.on('child_added', function(snapshot) {
+        [MESSAGE CALLBACK CODE GOES HERE]
+      });
+      function displayChatMessage(name, text) {
+        $('<div/>').text(text).prepend($('<em/>').text(name+': ')).appendTo($('#messagesDiv'));
+        $('#messagesDiv')[0].scrollTop = $('#messagesDiv')[0].scrollHeight;
+      };
 });
