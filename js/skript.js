2fd62e357ca179d2baa33ae1c71e7cf5eebc60b3
Vasiliy <i-hitman47@yandex.ru>
2015-09-02 02:43:21 +0300

    ывап

--- js/skript.js
+++ js/skript.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     var myDataRef = new Firebase('https://iwish.firebaseio.com/wishes');
     console.log("started");
  $('#wish').on('click', function() {
-    var wish = $('#wishInput').val();
-    myDataRef.child(wish).set({wish: wish});
+    var mywish = $('#wishInput').val();
+    myDataRef.child(mywish).set({wish: mywish});
     var message1 = '<div id="step">шаг 2 из 4</div><p>Вы загадали</p> </br> <p>чтобы ваше желание исполнилось, укажите:</p> <div id="field1"><p>Зачем?</p><input class="span2" id="reason" type="text" placeholder="причина"><button type="submit" class="btn btn-primary" id="add_reason" >добавить</button>  </div>';
     $('.hero-unit').html(message1);
      console.log("wish");
@@ -11,5 +11,5 @@ $(document).ready(function(){
   $('#add_reason').on('click', function() {
      //var myDataRef = new Firebase('https://iwish.firebaseio.com/wishes/' + wish + ');
   });
-    console.log(wish);
+    console.log(mywish);
 });
