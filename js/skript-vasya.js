1f7ad71c25f1e03d3698e18582f9c26e52efd9e2
hellaran <sir.hellaran@gmail.com>
2015-09-03 03:02:49 +0300

    rename

---
+++ js/skript-vasya.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    var myDataRef = new Firebase('https://iwish.firebaseio.com/wishes');
+    var online = 0;
+    var my_wish = "";
+    var div_wish = '<div id="wish_div"><h1>Привет!</h1> <p>Пришло время загадать своё первое желание!</p><input type="text" id="wishInput" placeholder="я хочу"><button type="submit" class="btn btn-primary" id="wish" >Захотеть &raquo;</button></div>';
+    var btn_id = "";
+    var btn_add = '<button type="submit" class="btn btn-primary" id="add" >добавить</button>';
+    var div_reason = '<div id="reasons_div"><p>Ваше желание:</p>' + my_wish + '<p>Для его исполнения, вам нужно указать, для чего вам это нужно.</p><input class="span2" id="reason" type="text" placeholder="причина"><button type="submit" class="btn btn-primary" id="add_reason" >добавить</button></div>';
+    var div_price = '';
+    var div_hedge = '';
+        if(online === 0){
+            var div_step = '<div id="step">шаг 1 из 4</div>';
+            $('.hero-unit').html(div_step + div_wish);
+        }
+     $('#wish').on('click', function() {
+        my_wish = $('#wishInput').val();
+        myDataRef.child(my_wish).set({wish: my_wish});
+        var div_step = '<div id="step">шаг 2 из 4</div>';
+        $('.hero-unit').html(div_step + div_reason);
+        console.log("reason");
+     });
+     $('#add_reason').on('click', function() {
+        console.log("mywish");
+        //var myDataRef = new Firebase('https://iwish.firebaseio.com/wishes/' + wish + ');
+     });
+});
--- js/skript.js
+++ js/skript.js
@@ -1,26 +1,76 @@
 $(document).ready(function(){
-    var myDataRef = new Firebase('https://iwish.firebaseio.com/wishes');
+    var wishDataRef = new Firebase('https://iwish.firebaseio.com/wishes');
+    var reasonDataRef = new Firebase('https://iwish.firebaseio.com/reasons');
     var online = 0;
     var my_wish = "";
-    var div_wish = '<div id="wish_div"><h1>Привет!</h1> <p>Пришло время загадать своё первое желание!</p><input type="text" id="wishInput" placeholder="я хочу"><button type="submit" class="btn btn-primary" id="wish" >Захотеть &raquo;</button></div>';
-    var btn_id = "";
-    var btn_add = '<button type="submit" class="btn btn-primary" id="add" >добавить</button>';
-    var div_reason = '<div id="reasons_div"><p>Ваше желание:</p>' + my_wish + '<p>Для его исполнения, вам нужно указать, для чего вам это нужно.</p><input class="span2" id="reason" type="text" placeholder="причина"><button type="submit" class="btn btn-primary" id="add_reason" >добавить</button></div>';
-    var div_price = '';
-    var div_hedge = '';
-        if(online === 0){
-            var div_step = '<div id="step">шаг 1 из 4</div>';
+    var my_reason= "";
+    var my_hedge= "";
+    var my_price= "";
+
+    var div_wish = '<div><h1>Привет!</h1> <p>Пришло время загадать своё первое желание!</p><input type="text" id="wishInput" placeholder="я хочу"><button type="submit" class="btn btn-primary" id="wish" >Захотеть &raquo;</button></div>';
+    var div_reason = '<div><h1>причина</h1><input type="text" id="reasonInput" placeholder="я хочу"><button type="submit" class="btn btn-primary" id="reason" >Захотеть &raquo;</button></div>';
+    var div_hedge = '<div><h1>преграда</h1><input type="text" id="hedgeInput" placeholder="я хочу"><button type="submit" class="btn btn-primary" id="hedge" >Захотеть &raquo;</button></div>';
+    var div_price = '<div><h1>жертва</h1><input type="text" id="priceInput" placeholder="я хочу"><button type="submit" class="btn btn-primary" id="price" >Захотеть &raquo;</button></div>';
+
+
+    if(online === 0){
+        var div_step = '<div>шаг 1 из 4</div>';
             $('.hero-unit').html(div_step + div_wish);
         }
      $('#wish').on('click', function() {
         my_wish = $('#wishInput').val();
-        myDataRef.child(my_wish).set({wish: my_wish});
-        var div_step = '<div id="step">шаг 2 из 4</div>';
+        wishDataRef.child(my_wish).set({wish: my_wish});
+        var div_step = '<div>шаг 2 из 4</div>';
         $('.hero-unit').html(div_step + div_reason);
-        console.log("reason");
+        console.log("wish");
      });
+
+    /*
+     $('#reason').on('click', function() {
+        my_reason = $('#reasonInput').val();
+        reasonDataRef.child(my_reason).set({reason: my_reason});
+        var div_step = '<div id="step">шаг 3 из 4</div>';
+        $('.hero-unit').html(div_step + div_hedge);
+        console.log("hedge");
+     });
+
+
+    */
+
+
+
+
+
+
+
+
+
+    /*
+
      $('#add_reason').on('click', function() {
-        console.log("mywish");
+        reason = $('#reasonInput').val();
+        myDataRef.child(my_wish).set({wish: my_wish});
+        var div_step = '<div id="step">шаг 3 из 4</div>';
+        $('.hero-unit').html(div_step + div_hedge);
+        console.log("hedge");
         //var myDataRef = new Firebase('https://iwish.firebaseio.com/wishes/' + wish + ');
      });
+
+
+
+
+     $('#add_hedge').on('click', function() {
+        hedge = $('#hedgeInput').val();
+        myDataRef.child(reason).set({reason: reason});
+        var div_step = '<div id="step">шаг 3 из 4</div>';
+        $('.hero-unit').html(div_step + div_hedge);
+        console.log("hedge");
+        //var myDataRef = new Firebase('https://iwish.firebaseio.com/wishes/' + wish + ');
+     });
+    */
+         /*    var btn_id = "";
+        var btn_add = '<button type="submit" class="btn btn-primary" id="add" >добавить</button>';*/
+
+
+
 });
