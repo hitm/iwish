8a1b59603bd00e77f72b14008f8fb0bf7be2a1cc
hellaran <sir.hellaran@gmail.com>
2015-09-09 14:43:39 +0300

    123

--- js/script2.js
+++ js/script2.js
@@ -7,7 +7,12 @@ $(document).ready(function(){
     var div1 = '<div id="one"><input type="text" id="text1" placeholder="wish"><button type="submit" id="btn1">add wish</button></div>';
     var div2 = '<div id="two"><input type="text" id="text2" placeholder="reason"><button type="submit" id="btn2">add reason</button><button type="submit" id="next">next</button></div><div id="list"></div>';
     var div3 = '<div id="three"><input type="text" id="text3" placeholder="hedge"><button type="submit" id="btn3">add3</button><button type="submit" id="next">next</button></div><div id="list"></div>';
-    var div4 = '<div id="four"><input type="text" id="text4" placeholder="piece""><button type="submit" id="btn4">add prices</button><button type="submit" id="finish">finish</button></div><div id="list"></div><div id="list2"></div><div id="list3"></div><div id="list4"></div>';
+    var div4 = '<div id="four"><input type="text" id="text4" placeholder="piece""><button type="submit" id="btn4">add prices</button><button type="submit" id="finish">finish</button></div><div id="list"></div><div id="list2"></div><div id="list3"></div><div id="list4"></div><div id="login"></div>';
+    var login = '<input type="email" class="form-control" id="email" placeholder="Email address" required autofocus><input type="password" class="form-control" id="pass" placeholder="Password" required> <br><label class="checkbox"><br><input type="checkbox" value="remember-me"> Remember me</label><br><button class="btn btn-lg btn-primary btn-block" id="reg">register</button><br><button class="btn btn-lg btn-primary btn-block" id="log">login</button><br>';
+
+
+
+
     var next_div = div1;
     myobject.addattr = function(tt) {
        var localcount = count + 1;
@@ -95,8 +100,47 @@ $(document).ready(function(){
           usersRef.set(testext);
         $('#list').html('');
         $('#list2').html("ваши причины:" + " " + myobject.attr2);
-         $('#list3').html("ваши преграды:" + " " + myobject.attr3);
-            $('#list4').html("ваши жертвы:" + " " + myobject.attr4);
+        $('#list3').html("ваши преграды:" + " " + myobject.attr3);
+        $('#list4').html("ваши жертвы:" + " " + myobject.attr4);
+        $('#login').html( login );
    	   });
+
+    var email = $('#email').val();
+    var password = $('#pass').val();
+
+    $("#container").on("click", '#btnreg', function(){
+        var email = $('#email').val();
+    var password = $('#pass').val();
+    DataRef.createUser({
+    email    : email,
+    password : password
+    }, function(error, userData) {
+         console.log("email");
+         console.log("password");
+        if (error) {
+       console.log("Error creating user:", error);
+       } else {
+       console.log("Successfully created user account with uid:", userData.uid);
+       }
+       });
+    });
+
+
+    $("#container").on("click", '#btnlog', function(){
+    var email = $('#email').val();
+    var password = $('#pass').val();
+    DataRef.authWithPassword({
+    email    : email,
+    password : password
+}, function(error, authData) {
+  if (error) {
+    console.log("Login Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+    console.log(authData.uid);
+  }
+});
+    });
+
 
 });
