f7a50a0cd0f0ab87514b92e4ec49c094ac8205e4
hellaran <sir.hellaran@gmail.com>
2015-09-10 17:14:54 +0300

    добавлена регистрация через почту

--- js/script2.js
+++ js/script2.js
@@ -11,10 +11,8 @@ $(document).ready(function () {
     var div2 = '<div id="two"><input type="text" id="text2" placeholder="reason"><button type="submit" id="btn2">add reason</button><button type="submit" id="next">next</button></div><div id="list"></div>';
     var div3 = '<div id="three"><input type="text" id="text3" placeholder="hedge"><button type="submit" id="btn3">add3</button><button type="submit" id="next">next</button></div><div id="list"></div>';
     var div4 = '<div id="four"><input type="text" id="text4" placeholder="piece""><button type="submit" id="btn4">add prices</button><button type="submit" id="finish">finish</button></div><div id="list"></div><div id="list2"></div><div id="list3"></div><div id="list4"></div><div id="login"></div>';
-    var div5 = '<input type="email" class="form-control" id="email" placeholder="Email address" required autofocus><input type="password" class="form-control" id="pass" placeholder="Password" required> <br><label class="checkbox"><br><input type="checkbox" value="remember-me"> Remember me</label><br><button class="btn btn-lg btn-primary btn-block" id="reg">register</button><br><button class="btn btn-lg btn-primary btn-block" id="log">login</button><br>';
+    var div5 = '<div id="five"><input type="email" class="form-control" id="email" placeholder="Email address" required autofocus><input type="password" class="form-control" id="pass" placeholder="Password" required> <br><label class="checkbox"><br><input type="checkbox" value="remember-me"> Remember me</label><br><button class="btn btn-lg btn-primary btn-block" id="btnreg">register</button><br><button class="btn btn-lg btn-primary btn-block" id="btnlog">login</button><br></div>';
     var div6 = '<div id="success">Success!</div>';
-
-
 
     var next_div = div1;
     myobject.addattr = function (tt) {
@@ -40,33 +38,33 @@ $(document).ready(function () {
 
 
         }
-//    записываем в контейнер желание
+        //    записываем в контейнер желание
     $('#container').html(div1);
 
-//    функция отправки желания
+    //    функция отправки желания
     $('#container').on('click', '#btn1', function () {
         var text = $('#text1').val();
         myobject.addattr(text);
     });
-//    функция добавления причины
+    //    функция добавления причины
     $('#container').on('click', '#btn2', function () {
         var text = $('#text2').val();
         myobject.addattr(text);
         $('#text2').val('');
     });
-//    функция добавления проблемы
+    //    функция добавления проблемы
     $('#container').on('click', '#btn3', function () {
         var text = $('#text3').val();
         myobject.addattr(text);
         $('#text3').val('');
     });
-//    функция добавления жертвы
+    //    функция добавления жертвы
     $('#container').on('click', '#btn4', function () {
         var text = $('#text4').val();
         myobject.addattr(text);
         $('#text4').val('');
     });
-//    функция отправки причины и проблемы
+    //    функция отправки причины и проблемы и перехода к следующему шагу
     $('#container').on('click', '#next', function () {
         if (next_div === div2) {
             next_div = div3;
@@ -82,7 +80,7 @@ $(document).ready(function () {
         $('#container').html(next_div);
         count = 0;
     });
-//    функция отправки жертвы
+    //    функция отправки жертвы, ввода информации в базу и перехода к регистрации
     $('#container').on('click', '#finish', function () {
         var usersRef = DataRef.child(myobject.attr1);
         var str = myobject.attr4;
@@ -100,39 +98,28 @@ $(document).ready(function () {
         console.log(json_prises);
 
         var testext = {
+            "reasons": json_reasons,
             "heges": json_hedges,
             "prices": json_prises,
-            "reasons": json_reasons
         };
         usersRef.set(testext);
         $('#list').html(myobject.attr1);
         $('#list2').html("ваши причины:" + " " + myobject.attr2);
         $('#list3').html("ваши преграды:" + " " + myobject.attr3);
         $('#list4').html("ваши жертвы:" + " " + myobject.attr4);
-        $('#login').html(div5);
-    });
-
-    var email = $('#email').val();
-    var password = $('#pass').val();
-
-
-
-
-    $('#container').on('click', '#btnreg', function () {
         if (next_div === div4) {
             next_div = div5;
-            console.log("начало регистрации");
+            console.log("перешли к 5");
+            $('#container').html(next_div);
+        } else {
+            console.log("какая-то ошибка");
         }
-        $('#container').html(next_div);
-
+        // $('#login').html(div5);
     });
+    //    функция регистрации
 
+    $('#container').on('click', '#btnreg', function () {
 
-
-
-
-
-        $("#container").on("click", '#btnreg', function () {
             console.log("начало регистрации");
             var email = $('#email').val();
             var password = $('#pass').val();
@@ -152,6 +139,7 @@ $(document).ready(function () {
 
 
         $("#container").on("click", '#btnlog', function () {
+            console.log("попытка входа");
             var email = $('#email').val();
             var password = $('#pass').val();
             DataRef.authWithPassword({
