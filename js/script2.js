5d66474d0aed73650c273a5bdf282e881fc22c66
hellaran <sir.hellaran@gmail.com>
2015-09-15 17:59:22 +0300

    сделал вторую тестовую кнопку

--- js/script2.js
+++ js/script2.js
@@ -53,27 +53,27 @@ $(document).ready(function () {
 
 
 
-        if (cookie === 'null' || typeof cookie === undefined ) {
-            console.log('пустые куки');
-            remember: "sessionOnly";
-            ref.authAnonymously(function (error, authData) {
-                if (error) {
-                    console.log("Login Failed!", error);
-                } else {
-                    console.log("Authenticated successfully with payload:", authData);
-                    authData.nick = 'Guest';
-                    authData.auth = null;
-                    ref.child(authData.uid).set(authData);
-                    console.log("смотрим содержимое объекта authData:", authData);
-                    console.log("смотрим содержимое authData.uid:", authData.uid);
-                }
-                $.cookie('userId', authData.uid);
-                cookie = $.cookie('userId');
-            });
-        } else {
-            console.log('есть куки', $.cookie('userId'));
-            console.log(cookie);
-        };
+    if (cookie === 'null' || typeof cookie === undefined) {
+        console.log('пустые куки');
+        remember: "sessionOnly";
+        ref.authAnonymously(function (error, authData) {
+            if (error) {
+                console.log("Login Failed!", error);
+            } else {
+                console.log("Authenticated successfully with payload:", authData);
+                authData.nick = 'Guest';
+                authData.auth = null;
+                ref.child(authData.uid).set(authData);
+                console.log("смотрим содержимое объекта authData:", authData);
+                console.log("смотрим содержимое authData.uid:", authData.uid);
+            }
+            $.cookie('userId', authData.uid);
+            cookie = $.cookie('userId');
+        });
+    } else {
+        console.log('есть куки', $.cookie('userId'));
+        console.log(cookie);
+    };
 
 
 
@@ -174,7 +174,7 @@ $(document).ready(function () {
             var outid = idSnapshot.val();
             console.log(outid);
             div_userpage = '<div class="div_userpage"><div id="user_info" class="col-md-12"><div id="avatar" class="col-md-2"><img src="http://lorempixel.com//100/150/people"></div><div id= "userhead" class="col-md-9"><h1>' + outid.name + '</h1><p>user_info</p><p>user_info</p></div><div id="rank" class="col-md-1"><img src="http://lorempixel.com/g/50/50/"><img src="http://lorempixel.com/g/50/50/"><img src="http://lorempixel.com/g/50/50/"></div></div><div id="my_wishes" class="col-md-6"><h1>my_wishes</h1><div id="accordeon" class="panel-group"><div class="panel panel-default"><div class="panel-heading"><span><h3 class="panel-title"><a href="#collapse-1" data-parent="#accordeon" data-toggle="collapse">открыть 1 слайд</a></h3></span></div><div id="collapse-1" class="panel-collapse collapse"><div class="panel-body"><p>1ыыыыыыыыыыыыыыыы</p></div></div></div><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title"><a href="#collapse-2" data-parent="#accordeon" data-toggle="collapse">открыть 2 слайд</a></h3></div><div id="collapse-2" class="panel-collapse collapse"><div class="panel-body"><p>2ыыыыыыыыыыыыыыыы</p></div></div></div><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title"><a href="#collapse-3" data-parent="#accordeon" data-toggle="collapse">открыть 3 слайд</a></h3></div><div id="collapse-3" class="panel-collapse collapse"><div class="panel-body"><p>3ыыыыыыыыыыыыыыыы</p></div></div></div></div></div><div class = "wish"><span></div></div><div id="others_wishes" class="col-md-6"><h1>other_wishes</h1><div id="accordeon2" class="panel-group"><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title"><a href="#collapse-4" data-parent="#accordeon2" data-toggle="collapse">открыть 1 слайд</a></h3></div><div id="collapse-4" class="panel-collapse collapse"><div class="panel-body"><p>1ыыыыыыыыыыыыыыыы</p></div></div></div><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title"><a href="#collapse-5" data-parent="#accordeon2" data-toggle="collapse">открыть 2 слайд</a></h3></div><div id="collapse-5" class="panel-collapse collapse"><div class="panel-body"><p>2ыыыыыыыыыыыыыыыы</p></div></div></div><div class="panel panel-default"><div class="panel-heading"><h3 class="panel-title"><a href="#collapse-6" data-parent="#accordeon2" data-toggle="collapse">открыть 3 слайд</a></h3></div><div id="collapse-6" class="panel-collapse collapse"><div class="panel-body"><p>3ыыыыыыыыыыыыыыыы</p></div></div></div></div></div></div>';
-        $('#container').html(div_userpage);
+            $('#container').html(div_userpage);
         });
     };
 
@@ -190,8 +190,24 @@ $(document).ready(function () {
         $('#container').html(div_userpage);
     });
 
-       $('.navbar').on('click', '#test2', function () {
+    //вторая тестовая кнопка
+    $('.navbar').on('click', '#test2', function () {
         console.log('тестируем вторую кнопку');
+        //при нажатии устанавливаем новое содержание старого пользователя
+        var Ref = new Firebase('https://iwish.firebaseio.com/users/');
+        Ref.child(cookie).once("value", function (snapshot, authData) {
+            console.log('все о пользователе', snapshot.val());
+      //      authData = 'test';
+            json_reasons = '123';
+            json_hedges = '456';
+            json_prises = '789';
+            var testext = {
+                "reasons": json_reasons,
+                "heges": json_hedges,
+                "prices": json_prises,
+            };
+            Ref.child(cookie).set(testext);
+        });
     });
 
     //переход на страницу пользователя
@@ -258,12 +274,6 @@ $(document).ready(function () {
 
     //    отправка жертвы, ввод информации в базу и переход к регистрации
     $('#container').on('click', '#finish', function () {
-        var Ref = new Firebase('https://iwish.firebaseio.com/users');
-        Ref.once("value", function (snapshot) {
-            var idSnapshot = snapshot.child(cookie);
-            //var outid = idSnapshot.val();
-        console.log('анонимный юзер' + idSnapshot);
-        });
 
         var str = myobject.attr4;
         str = str.substring(0, str.length - 1);
@@ -284,8 +294,8 @@ $(document).ready(function () {
             "heges": json_hedges,
             "prices": json_prises,
         };
-       // usersRef.set(testext);
-       // userRef.set(testext);
+        // usersRef.set(testext);
+        // userRef.set(testext);
         $('#list').html(myobject.attr1);
         $('#list2').html("ваши причины:" + " " + myobject.attr2);
         $('#list3').html("ваши преграды:" + " " + myobject.attr3);
@@ -300,10 +310,10 @@ $(document).ready(function () {
     });
 
     //анон вход
-     $('#container').on('click', '#btnanon', function () {
+    $('#container').on('click', '#btnanon', function () {
 
 
-     });
+    });
     //    регистрация
     $('#container').on('click', '#btnreg', function () {
         $('#forspin').html(spiner);
@@ -329,7 +339,7 @@ $(document).ready(function () {
                 if (remember === true) {
 
                     //эти куки заменяют куки анонима, нужно брать от него!!
- console.log('проверка перезаписи', cookie);
+                    console.log('проверка перезаписи', cookie);
                     $.cookie('userId', userData.uid);
                 } else {};
             }
